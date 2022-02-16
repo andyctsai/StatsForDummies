31bc694114c73fe2e7801a2634022e9c22055be1
andytsai <tsaiandyc@gmail.com>
2022-02-16 00:28:40 -0800

    Updates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.write("""
 # DATA 515 Project - Statistics for Dummies
@@ -13,14 +14,16 @@ if uploaded_file:
 
 whatDo = st.radio("What do you want to do?", ('Data Visualization', 'Statistics'))
 if whatDo == 'Data Visualization':
-    vizType = st.radio("What type of visualization do you want?", ('Scatterplot', 'Bar Chart'))
+    vizType = st.radio("What type of visualization do you want?", ('Histogram','Scatterplot', 'Bar Chart'))
+    df.hist(column="IQ")
+    plt.show()
 elif whatDo == 'Statistics':
-    statType = st.radio("What type of statistics do you want to perform?", ('Hypothesis Testing', 'Regression'))
+    statType = st.radio("What type of statistics do you want to perform?", ('Summary Statistics','Hypothesis Testing', 'Regression'))
     if statType == 'Hypothesis Testing':
         testType = st.radio("What type of test do you want to perform?", ('Z-test', 'T-test', 'ANOVA'))
         if testType == 'Z-test':
-             st.radio("1-sample or 2-sample Z-test", ('1-sample', '2-sample', 'ANOVA'))
+             st.radio("1-sample or 2-sample Z-test", ('1-sample', '2-sample'))
         if testType == 'T-test':
-             st.radio("1-sample or 2-sample Z-test", ('1-sample', '2-sample', 'ANOVA'))
+             st.radio("1-sample or 2-sample Z-test", ('1-sample', '2-sample'))
 if st.button('Click Me to Celebrate!'):
     st.balloons()
