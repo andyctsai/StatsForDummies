424966ef8bdda3e7fea9f6dd7f81aa44439ec68b
andytsai <tsaiandyc@gmail.com>
2022-02-16 00:13:18 -0800

    Updates

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ st.write("""
 # DATA 515 Project - Statistics for Dummies
 """)
 
-uploaded_file = st.file_uploader("Choose a CSV file")
+uploaded_file = st.file_uploader("Upload a CSV file")
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
     st.write("Data Table")
     st.dataframe(df)
 
-whatDo = st.radio("What do you want to do?", ('Data Visualization', 'Statistics'))
+whatDo = st.radio("What do you want to do?", ('Data Visualization', 'Statistics'), index=None)
 if whatDo == 'Data Visualization':
-    vizType = st.radio("What type of visualization do you want?", ('Scatterplot', 'Bar Chart'))
+    vizType = st.radio("What type of visualization do you want?", ('Scatterplot', 'Bar Chart'), index=None)
 elif whatDo == 'Statistics':
-    statType = st.radio("What type of statistics do you want to perform?", ('Hypothesis Testing', 'Regression'))
+    statType = st.radio("What type of statistics do you want to perform?", ('Hypothesis Testing', 'Regression'), index=None)
     if statType == 'Hypothesis Testing':
-        testType = st.radio("What type of test do you want to perform?", ('Z-test', 'T-test', 'ANOVA'))
+        testType = st.radio("What type of test do you want to perform?", ('Z-test', 'T-test', 'ANOVA'), index=None)
