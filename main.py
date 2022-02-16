19ed82fd2171bdba413aa6342062c216ff4f791e
andytsai <tsaiandyc@gmail.com>
2022-02-15 23:45:37 -0800

    Upload File

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.write("""
 # DATA 515 Project - Statistics for Dummies
 """)
 
 uploaded_file = st.file_uploader("Choose a file")
+st.dataframe(data=uploaded_file, width=None, height=None)
